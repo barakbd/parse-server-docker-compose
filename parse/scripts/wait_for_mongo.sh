84a628d2f0ef1b9ac33bb301d659e5b417aa51a9
barakbd <barakbd@outlook.com>
2019-01-09 23:03:16 -0800

    working compose file with wait_for_mongo script

--- parse/scripts/wait_for_mongo.sh
+++ parse/scripts/wait_for_mongo.sh
@@ -5,7 +5,7 @@ set -e
 
 # host="$1"
 # shift
-# cmd="$@"
+cmd="$@"
 
 # until mongo --username $MONGO_INITDB_DATABASE_OWNER --password $MONGO_INITDB_DATABASE_OWNER_PASSWORD --authenticationDatabase admin --host $host --port 27017 --eval 'quit()'; do
 until node /parse-server/scripts/wait_for_mongo.js; do
@@ -15,4 +15,4 @@ done
 
 # >&2 echo "Mongo is up - exit code ${$?} - executing command"
 >&2 echo "Mongo is up - exit code - executing command"
-# exec $cmd
+exec $cmd
